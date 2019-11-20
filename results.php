2001ffbff3f196103e46c6cdff8f9a4ab0ef08be
firecannons <lawf48@mst.edu>
2019-11-19 18:05:58 -0600

    Word frequency of text file is working!

--- results.php
+++ results.php
@@ -10,6 +10,8 @@
   if($dbCon === false){
     die("ERROR: Connection Not Established" . mysqli_connect_error());
   }
+
+  $words = file_get_contents($_FILES['fileToUpload']['tmp_name']);
 ?>
 
 
@@ -202,13 +204,99 @@
             <h1 class="h3 mb-0 text-gray-800">Analyze Book File</h1>
           </div>
 
+
           <!-- Content Row -->
           <div class="row">
-            <?php 
-            $words = file_get_contents($_FILES['fileToUpload']['tmp_name']);
-            $freq = array_count_values(str_word_count($words, 1));
-            print_r($freq);
-            ?>
+
+            <!-- Earnings (Monthly) Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Number of Words</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?php
+                        echo str_word_count($words, 0);
+                      ?></div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <!-- Earnings (Monthly) Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card border-left-success shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Number of Characters</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?php
+                        echo strlen($words);
+                      ?></div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+           
+          </div>
+
+          <!-- Content Row -->
+
+          <div class="row">
+          
+            <!-- Pie Chart -->
+            <div class="col-xl-4 col-lg-5">
+              <div class="card shadow mb-4">
+                <!-- Card Header - Dropdown -->
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                  <h6 class="m-0 font-weight-bold text-primary">Word Frequency</h6>
+                  <div class="dropdown no-arrow">
+                    <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
+                      <div class="dropdown-header">Dropdown Header:</div>
+                      <a class="dropdown-item" href="#">Action</a>
+                      <a class="dropdown-item" href="#">Another action</a>
+                      <div class="dropdown-divider"></div>
+                      <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                  </div>
+                </div>
+                <!-- Card Body -->
+                <div class="card-body">
+                  <div class="chart-pie pt-4 pb-2">
+                    <canvas id="myPieChart"></canvas>
+                  </div>
+                  <div class="mt-4 text-center small">
+                  <?php 
+                    $word_mapping = array_count_values(str_word_count($words, 1));
+                    $index = 0;
+                    foreach($word_mapping as $side=>$direc)
+                    {
+                        echo '<span class="mr-2">
+                        <i class="fas fa-circle " id="pie' . $index . '"></i>' . $side . '
+                      </span>
+                      ';
+                      $index = $index + 1;
+                    } ?>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- Content Row -->
+          <div class="row">
           </div>
 
           <!-- Content Row -->
@@ -286,6 +374,89 @@
   <!-- Page level custom scripts -->
   <script src="js/demo/chart-area-demo.js"></script>
   <!-- <script src="js/demo/chart-pie-demo.js"></script> -->
+
+
+  <script src="RandomColor/randomColor.js"></script>
+
+
+
+  <script>
+
+<?php 
+$index = 0;
+foreach($word_mapping as $side=>$direc)
+{
+  echo 'document.getElementById("pie' . $index . '").style.color = randomColor();';
+  $index = $index + 1;
+} ?>
+
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#858796';
+
+// Pie Chart Example
+var ctx = document.getElementById("myPieChart");
+var myPieChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: [
+    <?php 
+    foreach($word_mapping as $side=>$direc)
+    {
+        echo "\"" . $side . "\",";
+    } ?>
+    ],
+    /*labels: ["Direct", "Referral", "Social"],*/
+    datasets: [{
+      data: [
+        <?php 
+        foreach($word_mapping as $side=>$direc)
+        {
+            echo "\"" . $direc . "\",";
+        } ?>
+      ],
+      /*data: [55, 30, 15],*/
+      backgroundColor: [
+      <?php 
+      $index = 0;
+      foreach($word_mapping as $side=>$direc)
+      {
+        echo 'document.getElementById("pie' . $index . '").style.color,';
+        $index = $index + 1;
+      } ?>
+      ],
+      /*backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
+      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+      hoverBorderColor: "rgba(234, 236, 244, 1)",*/
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      caretPadding: 10,
+    },
+    legend: {
+      display: false
+    },
+    cutoutPercentage: 80,
+  },
+});
+
+</script>
+
+
+
+
+
+
+
 </body>
 
 </html>
